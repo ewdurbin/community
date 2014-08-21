fddd9c36ccc605ad77b3f93faca2de564a63aa7d
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-08-20 21:33:21 -0400

    be pretty

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 
+import os
 from distutils.core import setup
 
 requirements = ['Flask',
                 'requests']
 
+def read(fname):
+    try:
+        path = os.path.join(os.path.dirname(__file__), fname)
+        return open(path).read()
+    except IOError:
+        return ""
+
 setup(
     name='community',
-    version='1.0.0a1',
+    version='1.0.0a2',
     license='MIT',
     author="Ernest W. Durbin III",
     author_email='ewdurbin@gmail.com',
     description='merge together wellness checks to unify your shit',
+    long_description=read('README.rst'),
     url='https://github.com/ewdurbin/community',
     packages=['community'],
     scripts=[],
